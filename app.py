c1e5b009a040e963fe1f6e272f7830f070f898fe
Muhammad-Ammad3 <ammad0598@gmail.com>
2024-12-15 21:11:48 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+# print("Hello, World!")
+
+# a = 5
+# b = 6
+# c = 7
+
+# s = (a + b + c) / 2
+
+# area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+# print('The area of the triangle is %0.2f' %area)
+# x= 15
+# x = x% 3
+# print(x)
+
+# a = 7
+# b = 2
+
+# print ('Sum: ', a + b)  
+
+# print ('Subtraction: ', a - b)   
+
+# print ('Multiplication: ', a * b)  
+
+# print ('Division: ', a / b) 
+
+# print ('Floor Division: ', a // b)
+
+# print ('Modulo: ', a % b)  
+
+# print ('Power: ', a ** b) 
+
+# a = 10
+
+# b = 5 
+
+# a += b      # a = a + b
+
+# print(a)
+
+
+# a = 5
+
+# b = 2
+
+# print('a == b =', a == b)
+
+# print('a != b =', a != b)
+
+# print('a > b =', a > b)
+
+# print('a < b =', a < b)
+
+# print('a >= b =', a >= b)
+
+# print('a <= b =', a <= b)
